71c468b7b0a5b1c1eccfccae060a4be9243349ec
Mark Steve Samson <contact@marksteve.me>
2012-03-23 13:34:15 +0800

    Bumped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
   name='fboauth2',
-  version='0.0.1',
+  version='0.0.2',
   py_modules=['fboauth2'],
   author='Mark Steve Samson',
   author_email='contact@marksteve.me',
